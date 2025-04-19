82e3d1087ea5978a2868c365c7c1695260552ff0
Uanderson Ricardo <uandersonrfs@gmail.com>
2025-04-19 01:07:31 -0300

    feat: add controltypeid and eventid

--- controls.go
+++ controls.go
@@ -1,1 +1,47 @@
 package uiautomation
+
+type ControlTypeId uintptr
+
+const (
+	UIA_AppBarControlTypeId       ControlTypeId = 50040
+	UIA_ButtonControlTypeId       ControlTypeId = 50000
+	UIA_CalendarControlTypeId     ControlTypeId = 50001
+	UIA_CheckBoxControlTypeId     ControlTypeId = 50002
+	UIA_ComboBoxControlTypeId     ControlTypeId = 50003
+	UIA_CustomControlTypeId       ControlTypeId = 50025
+	UIA_DataGridControlTypeId     ControlTypeId = 50028
+	UIA_DataItemControlTypeId     ControlTypeId = 50029
+	UIA_DocumentControlTypeId     ControlTypeId = 50030
+	UIA_EditControlTypeId         ControlTypeId = 50004
+	UIA_GroupControlTypeId        ControlTypeId = 50026
+	UIA_HeaderControlTypeId       ControlTypeId = 50034
+	UIA_HeaderItemControlTypeId   ControlTypeId = 50035
+	UIA_HyperlinkControlTypeId    ControlTypeId = 50005
+	UIA_ImageControlTypeId        ControlTypeId = 50006
+	UIA_ListControlTypeId         ControlTypeId = 50008
+	UIA_ListItemControlTypeId     ControlTypeId = 50007
+	UIA_MenuBarControlTypeId      ControlTypeId = 50010
+	UIA_MenuControlTypeId         ControlTypeId = 50009
+	UIA_MenuItemControlTypeId     ControlTypeId = 50011
+	UIA_PaneControlTypeId         ControlTypeId = 50033
+	UIA_ProgressBarControlTypeId  ControlTypeId = 50012
+	UIA_RadioButtonControlTypeId  ControlTypeId = 50013
+	UIA_ScrollBarControlTypeId    ControlTypeId = 50014
+	UIA_SemanticZoomControlTypeId ControlTypeId = 50039
+	UIA_SeparatorControlTypeId    ControlTypeId = 50038
+	UIA_SliderControlTypeId       ControlTypeId = 50015
+	UIA_SpinnerControlTypeId      ControlTypeId = 50016
+	UIA_SplitButtonControlTypeId  ControlTypeId = 50031
+	UIA_StatusBarControlTypeId    ControlTypeId = 50017
+	UIA_TabControlTypeId          ControlTypeId = 50018
+	UIA_TabItemControlTypeId      ControlTypeId = 50019
+	UIA_TableControlTypeId        ControlTypeId = 50036
+	UIA_TextControlTypeId         ControlTypeId = 50020
+	UIA_ThumbControlTypeId        ControlTypeId = 50027
+	UIA_TitleBarControlTypeId     ControlTypeId = 50037
+	UIA_ToolBarControlTypeId      ControlTypeId = 50021
+	UIA_ToolTipControlTypeId      ControlTypeId = 50022
+	UIA_TreeControlTypeId         ControlTypeId = 50023
+	UIA_TreeItemControlTypeId     ControlTypeId = 50024
+	UIA_WindowControlTypeId       ControlTypeId = 50032
+)
--- events.go
+++ events.go
@@ -1,1 +1,43 @@
 package uiautomation
+
+type EventId uintptr
+
+const (
+	UIA_ActiveTextPositionChangedEventId                 EventId = 20036
+	UIA_AsyncContentLoadedEventId                        EventId = 20006
+	UIA_AutomationFocusChangedEventId                    EventId = 20005
+	UIA_AutomationPropertyChangedEventId                 EventId = 20004
+	UIA_ChangesEventId                                   EventId = 20034
+	UIA_Drag_DragCancelEventId                           EventId = 20027
+	UIA_Drag_DragCompleteEventId                         EventId = 20028
+	UIA_Drag_DragStartEventId                            EventId = 20026
+	UIA_DropTarget_DragEnterEventId                      EventId = 20029
+	UIA_DropTarget_DragLeaveEventId                      EventId = 20030
+	UIA_DropTarget_DroppedEventId                        EventId = 20031
+	UIA_HostedFragmentRootsInvalidatedEventId            EventId = 20025
+	UIA_InputDiscardedEventId                            EventId = 20022
+	UIA_InputReachedOtherElementEventId                  EventId = 20021
+	UIA_InputReachedTargetEventId                        EventId = 20020
+	UIA_Invoke_InvokedEventId                            EventId = 20009
+	UIA_LayoutInvalidatedEventId                         EventId = 20008
+	UIA_LiveRegionChangedEventId                         EventId = 20024
+	UIA_MenuClosedEventId                                EventId = 20007
+	UIA_MenuModeEndEventId                               EventId = 20019
+	UIA_MenuModeStartEventId                             EventId = 20018
+	UIA_MenuOpenedEventId                                EventId = 20003
+	UIA_NotificationEventId                              EventId = 20035
+	UIA_Selection_InvalidatedEventId                     EventId = 20013
+	UIA_SelectionItem_ElementAddedToSelectionEventId     EventId = 20010
+	UIA_SelectionItem_ElementRemovedFromSelectionEventId EventId = 20011
+	UIA_SelectionItem_ElementSelectedEventId             EventId = 20012
+	UIA_StructureChangedEventId                          EventId = 20002
+	UIA_SystemAlertEventId                               EventId = 20023
+	UIA_Text_TextChangedEventId                          EventId = 20015
+	UIA_Text_TextSelectionChangedEventId                 EventId = 20014
+	UIA_TextEdit_ConversionTargetChangedEventId          EventId = 20033
+	UIA_TextEdit_TextChangedEventId                      EventId = 20032
+	UIA_ToolTipClosedEventId                             EventId = 20001
+	UIA_ToolTipOpenedEventId                             EventId = 20000
+	UIA_Window_WindowClosedEventId                       EventId = 20017
+	UIA_Window_WindowOpenedEventId                       EventId = 20016
+)
