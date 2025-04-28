19d58de0ca17afc9c187493a66585d7c8e3745b3
lujihong <382217590@qq.com>
2025-04-28 21:32:34 +0800

    Merging PR conflicts

--- element.go
+++ element.go
@@ -114,6 +114,33 @@ func (elem *UIAutomationElement) SetFocus() error {
 	return nil
 }
 
+func (elem *UIAutomationElement) SetValue(value string) error {
+	valuePattern, err := elem.GetValuePattern()
+	if err != nil {
+		return err
+	}
+	defer valuePattern.Release()
+	return valuePattern.SetValue(value)
+}
+
+func (elem *UIAutomationElement) Invoke() error {
+	invokePattern, err := elem.GetInvokePattern()
+	if err != nil {
+		return err
+	}
+	defer invokePattern.Release()
+	return invokePattern.Invoke()
+}
+
+func (elem *UIAutomationElement) DoDefaultAction() error {
+	legacyAccessiblePattern, err := elem.GetLegacyAccessiblePattern()
+	if err != nil {
+		return err
+	}
+	defer legacyAccessiblePattern.Release()
+	return legacyAccessiblePattern.DoDefaultAction()
+}
+
 func (elem *UIAutomationElement) GetCurrentPattern(patternId PatternId) (*ole.IUnknown, error) {
 	var patternObject *ole.IUnknown
 
@@ -145,6 +172,22 @@ func (elem *UIAutomationElement) GetInvokePattern() (*UIAutomationInvokePattern,
 	}
 
 	return (*UIAutomationInvokePattern)(unsafe.Pointer(invokePattern)), nil
+}
+
+func (elem *UIAutomationElement) GetLegacyAccessiblePattern() (*UIAutomationLegacyAccessiblePattern, error) {
+	patternObject, err := elem.GetCurrentPattern(LegacyIAccessiblePatternId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	legacyAccessiblePattern, err := patternObject.QueryInterface(IID_IUIAutomationLegacyIAccessiblePattern)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return (*UIAutomationLegacyAccessiblePattern)(unsafe.Pointer(legacyAccessiblePattern)), nil
 }
 
 func (elem *UIAutomationElement) GetTextPattern() (*UIAutomationTextPattern, error) {
